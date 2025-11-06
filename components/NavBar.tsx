e506703727ae0f928756517786856465c91b37cb
Leonardo de Aquino <leonardo.aquinobraga@gmail.com>
2025-11-06 17:01:22 -0300

    fix: nav bar atualizada

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -30,10 +30,8 @@ const styles = StyleSheet.create({
   container: {
     position: "absolute",
     bottom: 0,
-    left: 0,
-    right: 0,
-    width: 390,
-    height: 73,
+    width: "100%",
+    height: 75,
     paddingVertical: 10,
     paddingHorizontal: 24,
     flexDirection: "row",
